ba6a96b0fba2bcdbceb87138721b00c7aeb714b4
Omar Elbaga <oelbaga@newworldgroup.com>
2025-09-23 10:09:32 -0400

    structure update

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>Homepage</div>;
+  return <div>Homepage</div>;
 }
